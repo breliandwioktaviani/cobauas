561e45644ce2e8c45acae6ca4c8a316fb5f28928
breliandwioktaviani <brelianoktaviani@gmail.com>
2025-12-06 10:43:13 +0700

    memperbaiki profil

--- app/src/main/java/com/example/cobauas/Login.kt
+++ app/src/main/java/com/example/cobauas/Login.kt
@@ -22,31 +22,30 @@ class Login : AppCompatActivity() {
             insets
         }
         val email = findViewById<EditText>(R.id.etEmail)
-        val password = findViewById<EditText>(R.id.etPassword)
+        val passwordField = findViewById<EditText>(R.id.etPassword)
 
         val button = findViewById<Button>(R.id.loginButton)
 
         button.setOnClickListener {
         val username = email.text.toString()
-        val password = password.text.toString()
+            val password = passwordField.text.toString()
             if (username == "Adi" && password == "123" || username == "awa" && password == "123"
                 || username == "bre" && password == "123") {
                 val inten = Intent(this, HomeActivity::class.java)
-                inten.putExtra(Username,username)
-                inten.putExtra(Password,password)
+                inten.putExtra(KEY_USERNAME, username)
+                inten.putExtra(KEY_PASSWORD, password)
                 startActivity(inten)
                 Toast.makeText(this, "Login Berhasil" , Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(this, "Login Gagal", Toast.LENGTH_SHORT).show()
 
             }
+
         }
+
     }
-    companion object KEY{
-const val Username = "username"
-        const val Password = "password"
+    companion object {
+        const val KEY_USERNAME = "key_username"
+        const val KEY_PASSWORD = "key_password"
     }
-
-
-
 }
--- app/src/main/java/com/example/cobauas/home/HomeFragment.kt
+++ app/src/main/java/com/example/cobauas/home/HomeFragment.kt
@@ -41,6 +41,25 @@ class HomeFragment : Fragment() {
         recycler.layoutManager = LinearLayoutManager(requireContext())
         recycler.adapter = adapter
 
+        val username = arguments?.getString("username")
+        val password = arguments?.getString("password")
+
+        val buttonProfilee = view.findViewById<ImageView>(R.id.imgProfile)
+        buttonProfilee.setOnClickListener {
+            val intent = Intent(requireContext(), ProfileActivity::class.java)
+            intent.putExtra(Login.KEY_USERNAME, username)
+            intent.putExtra(Login.KEY_PASSWORD, password)
+            startActivity(intent)
+        }
+
+        val logout = view.findViewById<ImageView>(R.id.Logout)
+
+        logout.setOnClickListener {
+            val intent = Intent(requireContext(), Login::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+
         adapter.onEditClick = { motor, pos ->
             val intent = Intent(requireContext(), EditMotor::class.java)
             intent.putExtra("position", pos)
@@ -52,11 +71,6 @@ class HomeFragment : Fragment() {
             intent.putExtra("catatan", motor.catatan)
 
             startActivityForResult(intent, 2)
-        }
-        val buttonLogout = view.findViewById<ImageView>(R.id.Logout)
-        buttonLogout.setOnClickListener {
-            val intent = Intent(requireContext(),Login::class.java)
-            startActivity(intent)
         }
         // Tombol Tambah
         val btnTambah = view.findViewById<ImageView>(R.id.fabAdd)
